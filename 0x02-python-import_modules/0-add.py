9ebf45fbfb420481819c77e569013de0180500fb
Mark-shija <plusmaxtechtz@gmail.com>
2024-05-19 06:35:44 -0700

    0-add.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,15 @@
 #!/usr/bin/python3
-
 from add_0 import add
+
 def main():
+    """ main calls module func
+
+    Args:
+        no args
+
+    Returns:
+        The return add()
+    """
     a = 1
     b = 2
     result = add(a,b)
