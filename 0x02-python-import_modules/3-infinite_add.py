47d60f5339ffa19debd543aa22a2e453493830de
Mark-shija <=plusmaxtechtz@gmail.com>
2024-06-02 22:33:28 +0300

    3-infinite_add.py

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
-
 if __name__ == "__main__":
-
     import sys
-    Answer = 0
+    sum = 0
     for arg in sys.argv:
         if arg != sys.argv[0]:
-            Answer = Answer + int(arg)
-
-    print(Answer)
+            result += int(arg)
+    print(sum)
