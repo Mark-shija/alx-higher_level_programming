a6fe1baa5a807bbd49f9cc794a14a21e2f30b3c1
Mark-shija <plusmaxtechtz@gmail.com>
2024-06-19 12:08:05 -0700

    10-factorial.js

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -6,5 +6,7 @@ if (isNaN(parseInt(process.argv[2]))) {
   console.log('Missing size');
 } else {
   const size = parseInt(process.argv[2]);
-  for (let i = 0; i < size; i++) { console.log('X'.repeat(size));
-  } }
+  for (let i = 0; i < size; i++) {
+    console.log('X'.repeat(size));
+  }
+}
