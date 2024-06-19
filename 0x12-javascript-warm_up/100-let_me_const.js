99620126a0f1dc13d05d342ad08fee296478dd4d
Mark-shija <plusmaxtechtz@gmail.com>
2024-06-19 12:32:11 -0700

    100-main.js

---
+++ 0x12-javascript-warm_up/100-let_me_const.js
@@ -0,0 +1,3 @@
+#!/usr/bin/node
+// modify value in another file
+myVar = 333;
--- 0x12-javascript-warm_up/13-add.js
+++ 0x12-javascript-warm_up/13-add.js
@@ -1,5 +1,6 @@
 #!/usr/bin/node
 // prints addition of two numbers
+
 exports.add = function(a , b) {
     return  parseInt(a) + parseInt(b);
 }
