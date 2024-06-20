9bc732b77c56992ea480bf4ea554fa9a5b985187
Mark-shija <plusmaxtechtz@gmail.com>
2024-06-20 13:37:50 -0700

    2-rectangle.js

---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+// checks for constructor class variable
+
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }	    
+  }
+};
