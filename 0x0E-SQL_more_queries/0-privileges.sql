06009ab0d42f454296a56a6ce22ec01e1a9f19f6
Mark-shija <plusmaxtechtz@gmail.com>
2024-07-01 00:11:41 -0700

    1-create_user.sql

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,15 +1,5 @@
--- Script to list all privileges of user_0d_1 and user_0d_2
-
--- Check and create users if they do not exist
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost';
-
--- Granting privileges to the users if they are newly created
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_2'@'localhost' WITH GRANT OPTION;
-
+-- Script to list all privileges of user_0d_1 and user_0d_
 -- List all privileges for user_0d_1
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
-
 -- List all privileges for user_0d_2
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
