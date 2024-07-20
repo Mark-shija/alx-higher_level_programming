a79e93498f8c5d6f744cae7fbfc0b007f38102f4
Mark-shija <plusmaxtechtz@gmail.com>
2024-07-20 06:37:05 -0700

    Replace an item in a list

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -6,5 +6,5 @@ def new_in_list(my_list, idx, element):
     # Replace the element at the specified index if the index is valid
     if 0 <= idx < len(my_list):
         new_list[idx] = element
-        
+    
     return new_list
